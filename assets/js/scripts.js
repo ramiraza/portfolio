0ed3be2c02a686b976c974400613522a5ad91a84
unsyllable <ali120610@gmail.com>
2017-05-17 16:04:05 +0000

    added bower | npm | devicons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,8 @@
 // preloader script
+
 $(document).ready(() => {
   $('body').fadeIn(1500).addClass('show')
 })
-// $(window).on('load', function(){
-//   setTimeout(function(){
-//     $('body').fadeIn('slow')
-//   }, 1000)
-// })
+
+var today = new Date();
+document.getElementById('currentYear').innerHTML = today.getFullYear()
