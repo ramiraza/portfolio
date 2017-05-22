df8da5167bb75797d2b48f52c1eeaa9b8be80902
unsyllable <ali120610@gmail>
2017-05-22 05:09:10 -0700

    added sticky nav and page fixtures

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-// preloader script 
+// preloader script
 /*global $*/
 var today = new Date();
 
@@ -6,10 +6,18 @@ var today = new Date();
 (function() {
   $(document).ready(function() {
     $('body').fadeIn(1500).addClass('show');
-    
     $('#year').html(today.getFullYear());
   });
-  
-})();
 
-console.log($('.navbar-default').height())
+  $(window).on('scroll', () => {
+    var $nav = $('.navbar2');
+
+    if($(window).scrollTop() > 250 ) {
+      $nav.fadeIn($(window).scrollTop() - 100)
+    }
+    else {
+      $nav.fadeOut($(window).scrollTop() - 100)
+    }
+  })
+
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,18 @@ var shell       = require('gulp-shell');
 var browserSync = require('browser-sync').create();
 
 // task for running jekyll shell commands
-gulp.task('build', shell.task(['jekyll serve --watch']));
+gulp.task('build', shell.task(['jekyll serve -w']));
 
 // task for auto refresh with browser-sync
 gulp.task('serve', () => {
   browserSync.init({
     server: {
       baseDir: '_site/'
-    }
+    },
+    reloadDebounce: 500
   })
-  gulp.watch('_site/**/*.*').on('change', browserSync.reload)
+  gulp.watch('_site/**/*.*')
+      .on('change', browserSync.reload)
 });
 
 gulp.task('default', ['build', 'serve']);
